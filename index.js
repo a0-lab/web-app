57aa1b8abe779fc6dae7fc9e5a23c33f4e93ae41
a0-lab <84154525+a0-lab@users.noreply.github.com>
2021-05-21 09:57:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use(
 
 app.get("/", async (req, res) => {
   try {
-       const summary = await axios.get(\`${API_URL}/total\`);
+       const summary = await axios.get(`${API_URL}/total`);
        res.render("home", {
          user: req.oidc && req.oidc.user,
          total: summary.data.total,
